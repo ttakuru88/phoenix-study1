369389d6dc6731192c18695d8c4e554d0828d93e
takuru <asaumi@kray.jp>
2018-04-21 21:50:34 +0900

    ユーザを作成できる

--- lib/phx_app_web/controllers/user_controller.ex
+++ lib/phx_app_web/controllers/user_controller.ex
@@ -1,14 +1,24 @@
 defmodule PhxAppWeb.UserController do
   use PhxAppWeb, :controller
 
-  alias PhxApp.User
+  alias PhxApp.{User, Repo}
 
   def index(conn, _params) do
     user_changeset = User.changeset(%User{}, _params)
     render conn, "index.html", user_changeset: user_changeset
   end
 
-  def create(conn, _params) do
-    render conn, "index.html"
+  def create(conn, %{"user" => user_params}) do
+    user_changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(user_changeset) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "ユーザー作ったぜ")
+        |> redirect(to: user_path(conn, :index))
+      {:error, user_changeset} ->
+        conn
+        |> put_flash(:error, "作成失敗")
+        |> render("index.html", user_changeset: user_changeset)
+    end
   end
 end
