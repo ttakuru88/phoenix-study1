5bacd226623fedd208f3afd7d458479944981f41
takuru <asaumi@kray.jp>
2018-04-21 21:33:16 +0900

    ユーザ作成のフォーム設置

--- lib/phx_app_web/controllers/user_controller.ex
+++ lib/phx_app_web/controllers/user_controller.ex
@@ -1,8 +1,11 @@
 defmodule PhxAppWeb.UserController do
   use PhxAppWeb, :controller
 
+  alias PhxApp.User
+
   def index(conn, _params) do
-    render conn, "index.html"
+    user_changeset = User.changeset(%User{}, _params)
+    render conn, "index.html", user_changeset: user_changeset
   end
 
   def create(conn, _params) do
