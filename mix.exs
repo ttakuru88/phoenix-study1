5818e225d9df872852dc28c291c44cee08ce55f4
takuru <asaumi@kray.jp>
2018-06-03 10:16:47 +0900

    comeoninの設定漏れ対応

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule PhxApp.Mixfile do
   def application do
     [
       mod: {PhxApp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :comeonin]
     ]
   end
 
