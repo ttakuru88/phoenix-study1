7c996db68ed79a216d78411964a9afb8100be6ba
takuru <asaumi@kray.jp>
2018-06-24 21:12:57 +0900

    チャットログ部分をvueで置き換え

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -4,52 +4,9 @@
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/web/endpoint.ex":
 import {Socket} from "phoenix"
+import Vue from 'vue/dist/vue.js';
 
 let socket = new Socket("/socket", {params: {token: window.userToken}})
-
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "lib/web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "lib/web/templates/layout/app.html.eex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/2" function
-// in "lib/web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, pass the token on connect as below. Or remove it
-// from connect if you don't care about authentication.
 
 socket.connect()
 
@@ -59,33 +16,28 @@ channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
-window.onload = () => {
-  const newMessageEl = document.getElementById('new-message')
-  const submitButtonEl = document.getElementById('send-message')
+const messages = new Vue({
+  el: '#messages',
+  data: {
+    messages: []
+  }
+})
 
-  submitButtonEl.addEventListener('click', () => {
-    const message = newMessageEl.value
-    if(message.length <= 0) {
-      return
-    }
+const newMessageEl = document.getElementById('new-message')
+const submitButtonEl = document.getElementById('send-message')
 
-    channel.push('speak', {message: message})
-    newMessageEl.value = ''
-  }, false)
+submitButtonEl.addEventListener('click', () => {
+  const message = newMessageEl.value
+  if(message.length <= 0) {
+    return
+  }
 
-  const messagesEl = document.getElementById('messages')
+  channel.push('speak', {message: message})
+  newMessageEl.value = ''
+}, false)
 
-  channel.on('speak', payload => {
-    let messageEl = document.createElement('li')
-    messageEl.innerText = payload.message
-
-    if(messagesEl.children.length > 0){
-      messagesEl.insertBefore(messageEl, messagesEl.children[0])
-    }
-    else {
-      messagesEl.appendChild(messageEl)
-    }
-  })
-}
+channel.on('speak', payload => {
+  messages.messages.unshift({text: payload.message})
+})
 
 export default socket
